ca1c8d75e3dc5a0d0d9bfde338fc84e17c01f2fe
Lorena-arr2025 <arrnan846@gmail.com>
2025-10-12 14:05:49 -0300

    Primer commit

---
+++ src/test/java/ar/edu/viajefacil/ViajefacilApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.viajefacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajefacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
